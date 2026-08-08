f70358da137bd790462b59ac1ff78abb45eada59
Justdhif <noob1234five@gmail.com>
2026-08-08 07:32:55 +0700

    fix: move serverless source entrypoint to src/serverless.ts to eliminate filename conflicts

--- apps/api/src/serverless.ts
+++ apps/api/src/serverless.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
+import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import type { Request, Response } from 'express';
 
